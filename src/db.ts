254c021a98d6858eec87653ca0cc283b2f01fb10
Gnarkill33 <gnarkill33@gmail.com>
2025-11-26 21:58:46 +0300

    fix: fix return type of uuid function to remove ts errors

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { randomUUID } from 'crypto';
 
-export const uuid = () => randomUUID();
+export const uuid = (): string => randomUUID();
 
 const userIds = Array.from({ length: 8 }, uuid);
 const artistIds = Array.from({ length: 8 }, uuid);
