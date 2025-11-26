53e3469c0bdb83bb2f4f2e91d3e727c9d92becfb
Gnarkill33 <gnarkill33@gmail.com>
2025-11-26 14:59:33 +0300

    feat: add error handling for getAll and getById requests

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { validate as uuidValidate } from 'uuid';
 import { db } from 'src/db';
 
 @Injectable()
@@ -9,6 +14,15 @@ export class UserService {
 
   findById(id: string) {
     const user = db.users.find((user) => user.id === id);
+
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('Invalid user ID');
+    }
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
     return user;
   }
 }
