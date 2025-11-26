a6dedf0ddcb21de531001dd23b9310e035f5595d
Gnarkill33 <gnarkill33@gmail.com>
2025-11-26 18:45:18 +0300

    feat: add put request for user entity

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,29 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  findAll() {
+  findAllUsers() {
     return this.userService.findAllUsers();
   }
 
   @Get('/:id')
-  findById(@Param('id') id: string) {
-    return this.userService.findById(id);
+  findUserById(@Param('id') id: string) {
+    return this.userService.findUserById(id);
   }
 
   @Post()
-  create(@Body() dto: CreateUserDto) {
-    return this.userService.create(dto);
+  createUser(@Body() dto: CreateUserDto) {
+    return this.userService.createUser(dto);
+  }
+
+  @Put('/:id')
+  updateUser(@Param('id') id: string, @Body() dto: UpdateUserDto) {
+    return this.userService.updateUser(id, dto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,13 @@
 import {
   BadRequestException,
+  ForbiddenException,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
 import { validate as uuidValidate } from 'uuid';
 import { db, uuid } from 'src/db';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -13,7 +15,7 @@ export class UserService {
     return db.users;
   }
 
-  findById(id: string) {
+  findUserById(id: string) {
     const user = db.users.find((user) => user.id === id);
 
     if (!uuidValidate(id)) {
@@ -27,7 +29,7 @@ export class UserService {
     return user;
   }
 
-  create(dto: CreateUserDto) {
+  createUser(dto: CreateUserDto) {
     if (!dto.login || !dto.password) {
       throw new BadRequestException('Required fields are missing');
     }
@@ -44,5 +46,31 @@ export class UserService {
     db.users.push(newUser);
 
     return newUser;
+  }
+
+  updateUser(id: string, dto: UpdateUserDto) {
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('Invalid user ID');
+    }
+
+    const existingUser = this.findUserById(id);
+
+    if (!existingUser) {
+      throw new NotFoundException('User not found');
+    }
+
+    if (existingUser.password !== dto.oldPassword) {
+      throw new ForbiddenException('Wrong password');
+    }
+
+    const updatedUser = {
+      ...existingUser,
+      password: dto.newPassword,
+      version: existingUser.version + 1,
+    };
+
+    db.users = db.users.map((user) => (user.id === id ? updatedUser : user));
+
+    return updatedUser;
   }
 }
