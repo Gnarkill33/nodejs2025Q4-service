70f0a50f5f462c995de3cdcb3ccab63c0cb17caf
Gnarkill33 <gnarkill33@gmail.com>
2025-11-28 00:12:41 +0300

    feat: add addToFavs and deleteFromFavs for album entity in favs

--- src/favs/favs.controller.ts
+++ src/favs/favs.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { FavsService } from './favs.service';
 
 @Controller('favs')
@@ -8,5 +8,27 @@ export class FavsController {
   @Get()
   findAllFavourites() {
     return this.favsService.findAllFavorites();
+  }
+
+  @Post('/track/:id')
+  addTrackToFavs(@Param('id') id: string) {
+    return this.favsService.addTrackToFavs(id);
+  }
+
+  @Delete('/track/:id')
+  @HttpCode(204)
+  deleteTrackFromFavs(@Param('id') id: string) {
+    return this.favsService.deleteTrackFromFavs(id);
+  }
+
+  @Post('/album/:id')
+  addAlbumToFavs(@Param('id') id: string) {
+    return this.favsService.addAlbumToFavs(id);
+  }
+
+  @Delete('/album/:id')
+  @HttpCode(204)
+  deleteAlbumFromFavs(@Param('id') id: string) {
+    return this.favsService.deleteAlbumFromFavs(id);
   }
 }
--- src/favs/favs.service.ts
+++ src/favs/favs.service.ts
@@ -1,5 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { db } from 'src/db';
+import { validate as uuidValidate } from 'uuid';
 
 @Injectable()
 export class FavsService {
@@ -15,5 +21,75 @@ export class FavsService {
         db.favorites.tracks.includes(track.id),
       ),
     };
+  }
+
+  addTrackToFavs(id: string) {
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('Invalid track ID');
+    }
+
+    const existingTrack = db.tracks.find((track) => track.id === id);
+
+    if (!existingTrack) {
+      throw new UnprocessableEntityException('Track not found');
+    }
+
+    db.favorites.tracks.push(existingTrack.id);
+
+    return {
+      message: 'Track added to favorites',
+      statusCode: 201,
+    };
+  }
+
+  deleteTrackFromFavs(id: string) {
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('Invalid track ID');
+    }
+
+    const existingTrack = db.favorites.tracks.find((trackId) => trackId === id);
+
+    if (!existingTrack) {
+      throw new NotFoundException('Track not found in favorites');
+    }
+
+    db.favorites.tracks = db.favorites.tracks.filter(
+      (trackId) => trackId !== id,
+    );
+  }
+
+  addAlbumToFavs(id: string) {
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('Invalid album ID');
+    }
+
+    const existingAlbum = db.albums.find((album) => album.id === id);
+
+    if (!existingAlbum) {
+      throw new UnprocessableEntityException('Album not found');
+    }
+
+    db.favorites.albums.push(existingAlbum.id);
+
+    return {
+      message: 'Album added to favorites',
+      statusCode: 201,
+    };
+  }
+
+  deleteAlbumFromFavs(id: string) {
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('Invalid album ID');
+    }
+
+    const existingAlbum = db.favorites.albums.find((albumId) => albumId === id);
+
+    if (!existingAlbum) {
+      throw new NotFoundException('Album not found in favorites');
+    }
+
+    db.favorites.albums = db.favorites.albums.filter(
+      (albumId) => albumId !== id,
+    );
   }
 }
