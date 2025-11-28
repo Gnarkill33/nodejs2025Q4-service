84e1c5ed47abaa54e2ff2b3036a83a32e027f8d6
Gnarkill33 <gnarkill33@gmail.com>
2025-11-28 22:43:32 +0300

    feat: install swagger and build swagger doc

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 export const PORT = Number(process.env.PORT) || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription('Home music library service')
+    .setVersion('1.0.0')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
+
   await app.listen(PORT);
 }
 bootstrap();
